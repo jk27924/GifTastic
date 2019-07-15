f60122b71c38a7782ce06466a4c502c30a9623dd
Jihong Kim <jk279242naver.com>
2019-07-15 17:15:21 -0500

    added if and else statement for click event function to change the data state.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,6 +43,19 @@ function displayHeroInfo() {
             $("#gifs-appear-here").append(gifDiv);
         };
 
+        // Click gif files to change data-state
+        $(".gif").on("click", function () {
+
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+
     });
 }
 
